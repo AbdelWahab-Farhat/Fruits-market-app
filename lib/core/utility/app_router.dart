24c10e28eb8a493b0c6f15a028fefb058bb3d73e
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-08-04 23:31:16 +0200

    added new views

--- lib/core/utility/app_router.dart
+++ lib/core/utility/app_router.dart
@@ -1,3 +1,5 @@
+import 'package:fresh_fruits/features/auth/presentation/views/sign_in_view.dart';
+import 'package:fresh_fruits/features/auth/presentation/views/sign_up_view.dart';
 import 'package:fresh_fruits/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:fresh_fruits/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
@@ -6,6 +8,8 @@ import 'package:go_router/go_router.dart';
 abstract class AppRouter {
   static const String splashViewPath = '/';
   static const String onboardingViewPath = '/onboarding_view';
+  static const String signInViewPath = '/sign_in_view';
+  static const String signUpViewPath = '/sign_up_view';
 
   static final appRouter = GoRouter(
     routes: [
@@ -16,6 +20,14 @@ abstract class AppRouter {
       GoRoute(
         path: onboardingViewPath,
         builder: (context, state) => const OnboardingView(),
+      ),
+      GoRoute(
+        path: signInViewPath,
+        builder: (context, state) => const SignInView(),
+      ),
+      GoRoute(
+        path: signUpViewPath,
+        builder: (context, state) => const SignUpView(),
       ),
     ],
   );
