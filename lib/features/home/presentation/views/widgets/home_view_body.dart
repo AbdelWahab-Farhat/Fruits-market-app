93c7a3f8c17dc2a694b24ee1c56732d5fd1bdd56
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-08-06 01:33:07 +0200

    add more button

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fresh_fruits/core/widgets/custom_filled_button.dart';
 import 'app_bar_section.dart';
 import 'category_section.dart';
 import 'custom_sub_title.dart';
@@ -33,8 +34,18 @@ class HomeViewBody extends StatelessWidget {
                 subTitle: 'Trending Deals',
               ),
             ),
-            SliverFillRemaining(
+            SliverToBoxAdapter(
               child: TrendingSection(),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 28,vertical: 30),
+                child: CustomFilledButton(
+                  title: 'More',
+                  color: Colors.black,
+
+                ),
+              ),
             ),
           ],
         ),
