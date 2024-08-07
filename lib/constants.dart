e65745dc67a7c92aaf85bae99f9ebeb8d4e417fa
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-08-07 05:52:17 +0200

    added border color

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,5 +4,6 @@ const kLightYellow = Color(0xffFEC54B);
 const kOrange = Color(0xffE67F1E);
 const kLightGrey = Color(0xffF2F2F2);
 const kLightWhite = Color(0xffFAFEFC);
+const kBorderColor = Color(0xffCACACA);
 const G0 = Color(0xff12B76A);
 const kLinearGradientOrange = LinearGradient(colors: [Color(0xffF4AD16), Color(0xffE67F1E)]);
