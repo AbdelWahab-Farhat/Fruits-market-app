b48708c004317cedd2a2607695639dbf2a17868d
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-08-15 01:30:59 +0200

    root view

---
+++ lib/features/root/root.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:fresh_fruits/constants.dart';
+import 'package:fresh_fruits/features/cart/presentation/views/cart_view.dart';
+import 'package:fresh_fruits/features/categories/presentation/views/categories_view.dart';
+import 'package:fresh_fruits/features/home/presentation/views/home_view.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+
+class Root extends StatefulWidget {
+
+   Root({super.key});
+
+  @override
+  State<Root> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+   int _selectedIndex = 0;
+
+  final List<Widget> _views = [
+    const HomeView(),
+    const CategoriesView(),
+    const CartView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: GNav(
+          onTabChange: (value) {
+            _selectedIndex = value;
+            setState(() {});
+          },
+          selectedIndex: _selectedIndex,
+          tabBorderRadius: 30,
+          backgroundColor: const Color(0xffFAFEFC),
+          tabActiveBorder: Border.all(color: kLightGrey, width: 1), // tab button border
+          curve: Curves.easeOutExpo, // tab animation curves
+          duration: const Duration(milliseconds: 400), // tab animation duration
+          gap: 4, // the tab button gap between icon and text
+          color: Colors.grey[800], // unselected icon color
+          activeColor: kOrange, // selected icon and text color
+          iconSize: 24, // tab button icon size
+          tabBackgroundColor: Colors.orange.withOpacity(0.1), // selected tab background color
+          tabMargin: const EdgeInsets.only(left: 10 , right: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          tabs: const [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.category,
+              text: 'Categories',
+            ),
+            GButton(
+              icon: Icons.shopping_cart,
+              text: 'Cart',
+            ),
+          ]
+      ),
+      body: _views[_selectedIndex],
+    );
+  }
+}
