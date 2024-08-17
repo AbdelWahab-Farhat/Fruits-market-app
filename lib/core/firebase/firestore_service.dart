10f2701bf25af8f318e850bf9bb7813680a70d76
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-08-17 21:31:46 +0200

    update's

--- lib/core/firebase/firestore_service.dart
+++ lib/core/firebase/firestore_service.dart
@@ -1,18 +1,46 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
+import 'package:fresh_fruits/core/models/customer.dart';
+import 'package:uuid/uuid.dart';
 import '../error/failure.dart';
 import '../models/item.dart';
 
 class FireStoreService {
   // Collections Names
   static const items_Collection = 'Items';
+  static const users_Collection = 'Users';
 
-  Future<Either<Failure,List<Item>>> fetchItems()async{
+  Future<Either<Failure, List<Item>>> fetchItems() async {
     try {
-      final snapShot = await FirebaseFirestore.instance.collection(items_Collection).get();
-      return right(snapShot.docs.map((e) => Item.fromJson(e.data()),).toList());
-    } on FirebaseException catch(e) {
+      final snapShot =
+          await FirebaseFirestore.instance.collection(items_Collection).get();
+      return right(snapShot.docs
+          .map(
+            (e) => Item.fromJson(e.data()),
+          )
+          .toList());
+    } on FirebaseException catch (e) {
       return left(ServerFailure(errMessage: e.message ?? e.code));
+    }
+  }
+
+  Future<bool> addUserInfoToDataBase(
+      {required String firstName,
+      required String lastName,
+      required String email}) async {
+    Customer customer = Customer(
+        ID: const Uuid().v4(),
+        firstName: firstName,
+        lastName: lastName,
+        email: email);
+    try {
+      await FirebaseFirestore.instance
+          .collection(users_Collection)
+          .doc(customer.ID)
+          .set(customer.toJson());
+      return true;
+    } on FirebaseException catch (_) {
+      return false;
     }
   }
 }
