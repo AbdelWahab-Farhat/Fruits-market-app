c8eded8d6372c91d6dbf00050b1e74c420e97be8
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-08-31 13:34:17 +0200

    update's

--- lib/features/item_details/presentation/views/widgets/custom_tab_bar.dart
+++ lib/features/item_details/presentation/views/widgets/custom_tab_bar.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fresh_fruits/core/widgets/custom_outlined_button.dart';
 import 'package:fresh_fruits/features/item_details/presentation/views/widgets/rating_section.dart';
-import 'package:go_router/go_router.dart';
 import '../../../../../constants.dart';
 import '../../../../../core/utility/app_style.dart';
-import '../../../../../core/utility/helper.dart';
-import '../../../../../core/widgets/custom_loading_widget.dart';
-import '../../../../auth/presentation/manger/sign_in_cubit/sign_in_cubit.dart';
-import '../../manger/item_details_cubit/item_details_cubit.dart';
 import 'comment_section.dart';
+import 'pay_des_section.dart';
 
 class CustomTabBar extends StatelessWidget {
   const CustomTabBar({
@@ -18,7 +12,6 @@ class CustomTabBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var item = context.read<ItemDetailsCubit>().item;
     return DefaultTabController(
       length: 3,
       child: Column(
@@ -53,53 +46,3 @@ class CustomTabBar extends StatelessWidget {
   }
 }
 
-class PayDesSection extends StatelessWidget {
-  const PayDesSection({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    var itemCubit = context.read<ItemDetailsCubit>();
-    var signInCubit = context.read<SignInCubit>();
-    return BlocConsumer<ItemDetailsCubit, ItemDetailsState>(
-      listener: (context, state) {
-        if (state is ItemDetailsSuccess) {
-          showTopSnackBar(context, "Item Added To Cart Successfully");
-          GoRouter.of(context).pop();
-        }
-        if (state is ItemDetailsFailed) {
-          showTopSnackBar(context, state.errMessage);
-        }
-      },
-      builder: (context, state) {
-        if (state is ItemDetailsLoading) {
-          return const CustomLoadingWidget();
-        }
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              itemCubit.item.description!,
-              style: AppStyle.style14,
-              maxLines: 4,
-            ),
-            const Spacer(),
-            CustomOutlinedButton(
-              title: 'Payment\n \$${itemCubit.item.price * itemCubit.quantity}',
-              onTap: () async {
-                await itemCubit.addItemToCart(signInCubit.customer!);
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
